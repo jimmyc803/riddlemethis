26f8cbf810e4936adf8ad9f36fa1f5905d5c8c29
Jimmy <155778038+jimmyc803@users.noreply.github.com>
2025-04-14 20:20:37 -0400

    make sure web page stays

--- js/landingScript.js
+++ js/landingScript.js
@@ -1,10 +1,17 @@
-window.addEventListener('pageshow', () => {
-    document.body.classList.remove('fade-out');
-});
-
-function slideAndRedirect(targetUrl) {
+window.addEventListener('pageshow', (event) => {
+    // This handles when you go "back" to the page and prevents it from being hidden
+    if (event.persisted) {
+      document.body.classList.remove('slide-up');
+    } else {
+      // Also remove in case the page loads fresh
+      document.body.classList.remove('slide-up');
+    }
+  });
+  
+  function slideAndRedirect(targetUrl) {
     document.body.classList.add("slide-up");
     setTimeout(() => {
-        window.location.href = targetUrl;
-    }, 700); // duration matches CSS
-}
+      window.location.href = targetUrl;
+    }, 700);
+  }
+  
